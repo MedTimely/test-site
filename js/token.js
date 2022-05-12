8993cebf451e665353b2d3bb3912a186c45dc04c
thefallnn <thefallnn@pm.me>
2022-05-12 14:04:37 +0530

    js:token: move: constant obj from form

--- js/token.js
+++ js/token.js
@@ -6,8 +6,8 @@ let char = `123abcde.fmnopqlABCDE@FJKLMNOPQRSTUVWXYZ456789stuvwxyz0!#$%&ijkrgh'*
 const generateToken = (key) => {
 
     let token = '';
-    for(let i = 0; i< key.length; i++){
-        let index = char.indexOf(key[i]) || char.length /2;
+    for(let i = 0; i < key.length; i++){
+        let index = char.indexOf(key[i]) || char.length / 2;
         let randomIndex =  Math.floor(Math.random() * index);
         token += char[randomIndex] + char[index - randomIndex];
     }
@@ -16,12 +16,51 @@ const generateToken = (key) => {
 
 const compareToken = (token, key) => {
     let string = '';
-    for(let i = 0; i < token.length;i++){
+    for(let i = 0; i < token.length;i=i+2){
         let index1 = char.indexOf(token[i]);
         let index2 = char.indexOf(token[i+1]);
         string += char[index1 + index2];
 
     }
-    return string === key;
-
+    if(string === key){
+        return true;
+    }
+        return false;
 }
+//common functions
+
+// send data  function
+const sendData=(path, data) =>{
+    fetch(path,{
+        method:'post',
+        headers: new Headers({'Content-Type':'application/json'}),
+        body: JSON.stringify(data)
+    }) .then((res) => res.json())
+        .then(response => {
+            processData(response);
+        })
+}
+const processData = (data) =>{
+    loader.style.display = null;
+    if (data.alert){
+        showAlert(data.alert);
+    } else if (data.name){
+        //create authToken
+        data.authToken = generateToken(data.email);
+        sessionStorage.user = JSON.stringify(data);
+        location.replace('/');
+    }
+}
+// calling alert function
+
+const showAlert = (msg) =>{
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() =>{
+        alertBox.classList.remove('show');
+    },2000);
+}
+
+ 
